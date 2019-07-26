245eac8aa36dc14313dedf3c1b3d258e0289d6a6
Mehmet Şensoy <51136928+MSensoy@users.noreply.github.com>
2019-07-26 16:03:02 +0300

    A

---
+++ DemoCoreProject/Demo.Northwind.MvcUI/ViewComponents/UserSummaryViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Demo.Northwind.MvcUI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Demo.Northwind.MvcUI.ViewComponents
+{
+    public class UserSummaryViewComponent : ViewComponent
+    {
+        public ViewViewComponentResult Invoke()
+        {
+
+            UserDetailsViewModel model = new UserDetailsViewModel
+            {
+                UserName = HttpContext.User.Identity.Name
+            };
+            return View(model);
+        }
+    }
+}
